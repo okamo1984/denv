252ec404f5c50d1709a1ee60121b9dd3372c48e7
okamo <okamo1984@gmail.com>
2024-09-05 22:05:52 +0900

    Add biome in LSP

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,22 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        bashls = {},
-        typos_lsp = {},
-        yamlls = {
-          settings = {
-            yaml = {
-              schemas = {
-                ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-              },
-            },
-          },
-        },
-        zls = {},
-      },
-    },
-  },
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				bashls = {},
+				biome = {},
+				typos_lsp = {},
+				yamlls = {
+					settings = {
+						yaml = {
+							schemas = {
+								["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+							},
+						},
+					},
+				},
+				zls = {},
+			},
+		},
+	},
 }
