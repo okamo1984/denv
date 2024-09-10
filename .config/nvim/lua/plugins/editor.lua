6b98499bdab64bc5c7c572edc9b9f2c01f068028
okamo <okamo1984@gmail.com>
2024-09-10 23:01:53 +0900

    Change neo-tree position to right

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,43 +1,51 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      {
-        "nvim-telescope/telescope-live-grep-args.nvim",
-        -- This will not install any breaking changes.
-        -- For major updates, this must be adjusted manually.
-        version = "^1.0.0",
-      },
-    },
-    config = function()
-      vim.keymap.set(
-        "n",
-        "<leader>fa",
-        ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-        { desc = "Live Grep Args" }
-      )
-      local telescope = require("telescope")
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				-- This will not install any breaking changes.
+				-- For major updates, this must be adjusted manually.
+				version = "^1.0.0",
+			},
+		},
+		config = function()
+			vim.keymap.set(
+				"n",
+				"<leader>fa",
+				":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+				{ desc = "Live Grep Args" }
+			)
+			local telescope = require("telescope")
 
-      -- first setup telescope
-      telescope.setup({
-        -- your config
-      })
+			-- first setup telescope
+			telescope.setup({
+				-- your config
+			})
 
-      -- then load the extension
-      telescope.load_extension("live_grep_args")
-    end,
-  },
-  {
-    "ruifm/gitlinker.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "<leader>gy", "<cmd>lua require('gitlinker').get_buf_range_url('n')<cr>", desc = "Copy Git Link" },
-      {
-        "<leader>gY",
-        "<cmd>lua require('gitlinker').get_buf_range_url('n', {action_callback = require('gitlinker.actions').open_in_browser})<cr>",
-        desc = "Open Git Link",
-      },
-    },
-    opts = {},
-  },
+			-- then load the extension
+			telescope.load_extension("live_grep_args")
+		end,
+	},
+	{
+		"ruifm/gitlinker.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>gy", "<cmd>lua require('gitlinker').get_buf_range_url('n')<cr>", desc = "Copy Git Link" },
+			{
+				"<leader>gY",
+				"<cmd>lua require('gitlinker').get_buf_range_url('n', {action_callback = require('gitlinker.actions').open_in_browser})<cr>",
+				desc = "Open Git Link",
+			},
+		},
+		opts = {},
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			window = {
+				position = "right",
+			},
+		},
+	},
 }
